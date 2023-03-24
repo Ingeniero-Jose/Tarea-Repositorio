5e6dc45cc5dba2467484e5ae7b32365df6cc5a8e
jose_lopez <joseykalila7@gmail.com>
2023-03-24 11:39:59 -0700

    Se crea repositorio

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class FactorialPrueba
+{
+   public static void Main(string[] args)
+   {
+      for (long contador = 0; contador <= 10; contador++)
+         Console.WriteLine("{0}!={1}", contador, Factorial(contador));
+   }
+
+   public static long Factorial (long numero)
+   {
+      if (numero <= 1)
+         return 1;
+
+      else 
+         return numero * Factorial (numero-1);
+   }
+}
